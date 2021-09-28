50df54b82de1e8e4fefd4a8e0c70fcf226801e93
MattTriano <matt.triano@gmail.com>
2021-09-28 17:39:59 -0500

    Added steps to generate the tiger.loader_generate_nation_script() script, and then modify that script to actually work.

--- init_files/initdb-postgis.sh
+++ init_files/initdb-postgis.sh
@@ -23,3 +23,10 @@ for DB in template_postgis "$POSTGRES_DB"; do
 
 EOSQL
 done
+
+psql -c "SELECT Loader_Generate_Nation_Script('sh')" -d geocoder -tA > /gisdata/nation_script_load.sh
+sed -i 's,PGUSER=postgres,PGUSER='"`cat /run/secrets/postgresql_user`"',g' /gisdata/nation_script_load.sh
+sed -i 's,PGPASSWORD=yourpasswordhere,PGPASSWORD='"`cat /run/secrets/postgresql_password`"',g' /gisdata/nation_script_load.sh
+sed -i 's,PGDATABASE=geocoder,PGDATABASE='"`cat /run/secrets/postgresql_db`"',g;' /gisdata/nation_script_load.sh
+sed -i 's,${PSQL},${PSQL} -d ${PGDATABASE} -U ${PGUSER} -h ${PGHOST},g;' /gisdata/nation_script_load.sh
+# sh /gisdata/nation_script_load.sh 
